20344c5bdb9dfd101d5d76e2a9b30ec40a6405cb
Arjun Balasubramanian <arjunbala0711@gmail.com>
2019-11-11 18:18:45 -0600

    Make interfaces more clear

--- src/csbtree.h
+++ src/csbtree.h
@@ -5,23 +5,79 @@
 using namespace std;
 
 /**
+ * Representation of a CSB Tree node
+ */
+class CSBNode
+{
+    private:
+        uint64_t *data; // Assume 64-bit integer primary key index
+        bool ifLeaf; // Indicates if this node is a leaf node
+        CSBNode *left_child; // Pointer to leftmost child
+        int nKeys; // Number of keys currently held by this node
+
+    public:
+
+	/**
+	 * Init a new CSB node given the order of the CSB tree
+	 */
+	CSBNode(int order)
+	{
+	    // TODO: Implement
+	    // Allocate data structures
+	}
+	/**
+	 * Add key to node.
+	 * Return 0 on success, -1 on failure
+	 */
+	int addKeyToNode(uint64_t key)
+	{
+	    // TODO: Implement
+	    return 0;
+	}
+
+	/**
+	 * Delete key from node
+         * Return 0 on success, -1 on failure
+	 */
+	int deleteKeyFromNode(uint64_t key)
+	{
+	    // TODO: Implement
+	    return 0;
+	}
+
+	/**
+	 * Check if key is present in this node
+	 * Return true if present, false if not
+	 */
+	bool checkPresenceOfKey(uint64_t key)
+	{
+            // TODO: Implement
+	    return true;
+	}
+
+	uint64_t getMaxValue()
+	{
+	    // TODO: Implement
+	    return 0;
+	}
+};
+
+/**
  * CSB Tree Base Class
  */
 class CSBTree
 {
     private:
-    	uint64_t *data; // Assume 64-bit integer primary key index
-    	bool ifLeaf; // Indicates if this node is a leaf node
-    	CSBTree *left_child; // Pointer to leftmost child
-    	int n; // Number of values that can be held by this node
+    	CSBTree *root; // Pointer to the root of the CSB tree
+    	int d; // order of the CSB tree
 
     public:
 	/**
-	 * Init a new CSB tree with n values possible within each node
+	 * Init a new CSB tree with the given order
 	 */
-	CSBTree(int num)
+	CSBTree(int order)
 	{
-	    n = num;
+	    d = order;
 	}
 
 	/**
@@ -33,12 +89,16 @@ class CSBTree
 	/**
 	 * Search for a given key
 	 */
-	int search(uint64_t key);
+	int search(uint64_t key)
+	{
+	    // TODO: Implement
+	    return 0;
+	}
 
 	/**
 	 * Delete a key index from the CSB tree
 	 */
-	int del(uint64_t key);
+	virtual int del(uint64_t key) = 0;
 
 	/**
 	 * Print the CSB tree
@@ -46,5 +106,6 @@ class CSBTree
 	void print()
 	{
 	    cout<<"Printing!"<<endl;
+	    // TODO: Implement
 	}
 };
--- src/fullcsbtree.h
+++ src/fullcsbtree.h
@@ -8,9 +8,19 @@ class FullCSBTree : public CSBTree
     public:
         FullCSBTree(int num) : CSBTree(num)
 	{
+	    // TODO: Implement
+	    // Init the tree
 	}
 
 	int insert(uint64_t key)
 	{
+	    // TODO: Implement
+	    return 0;
 	}
+
+	int del(uint64_t key)
+        {
+            // TODO: Implement
+            return 0;
+        }
 };
